c28262831da763800a7c31275ebd034a50b4261d
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-05-04 22:16:34 +0800

    added CORS

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
-const express = require('express');
-let wordList = require('./words').words();
+const express = require("express");
+let wordList = require("./words").words();
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+  return array;
 }
 
-
-app.get('/api/:count', (req, res) => {
-    shuffleList = shuffle(wordList);
-    let count = req.params.count;
-    count = parseInt(count, 10);
-    if (count === parseInt(count, 10)) {
-        if (count > shuffleList.length) {
-            count = shuffleList.length;
-        }
-        result = {
-            words: shuffleList.slice(0, count)
-        };
-        return res.json(result);
-    } else {
-        return res.status(400).json({ msg: 'The parameter should be an interger value' });
+app.get("/api/:count", (req, res) => {
+  shuffleList = shuffle(wordList);
+  let count = req.params.count;
+  count = parseInt(count, 10);
+  if (count === parseInt(count, 10)) {
+    if (count > shuffleList.length) {
+      count = shuffleList.length;
     }
+    result = {
+      words: shuffleList.slice(0, count)
+    };
+    return res.json(result);
+  } else {
+    return res
+      .status(400)
+      .json({ msg: "The parameter should be an interger value" });
+  }
 });
 
 const port = process.env.PORT || 5000;
